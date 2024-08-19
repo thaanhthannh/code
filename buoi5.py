3cddedea393dd36b488d6ecef06a131eb865040a
thaanhthannh <thanhthanh.cv.1707@gmail.com>
2024-08-19 20:31:49 +0700

    Add files via upload

---
+++ buoi5.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 18:05:08 2024
+
+@author: Student
+"""
+"""
+#Cach chuyen kieu du lieu
+var1=10
+print("Gia tri var1=", var1, "\n\tKieu: ", type(var1))
+var2=float(var1)
+print("Gia tri var2=", var2, "\n\tKieu: ", type(var2))
+var3= 3+4j
+print("Gia tri var3=", var3, "\n\tKieu: ", type(var3))
+var4=complex(var2)
+print("Gia tri var4=", var4, "\n\tKieu: ", type(var4))
+"""
+"""
+#Bai1
+a=int(input("Nhap a:"))
+b=int(input("Nhap b:"))
+c=int(input("Nhap c:"))
+d=int(input("Nhap d:"))
+e=(a+b+c+d)/4
+print(e)
+"""
+"""
+#Bai2
+a=int(input("Nhap a:"))
+b=int(input("Nhap b:"))
+print("Tong=", a+b)
+print("Hieu=", a-b)
+print("Tich=", a*b)
+print("Thuong=", a/b, round(a/b,2), round(a/b,3))
+print("Chia lay du=", a%b)
+print("Chia lay nguyen=", a//b)
+"""
+"""
+#Bai3
+n=int(input("Nhap so:"))
+if 10 <= n <= 99:
+    print("Tong cac chu so:", (n//10)+(n%10))
+else:
+    print("Khong xac dinh")
+"""
+"""
+#Bai4
+gio=int(input("Nhap gio:")); print("Tong so giay:", gio*3600)
+phut=int(input("Nhap phut:")); print("Tong so giay:", phut*60)
+giay=int(input("Nhap giay:"))
+"""
+"""
+#Bai5
+nam_sinh=int(input("Nhap nam sinh:"))
+tuoi= 2024 - nam_sinh
+print("Ban sinh nam", nam_sinh, "vay ban", tuoi, "tuoi")
+"""
+"""
+#Bai6
+a=int(input("Nhap he so a:"))
+b=int(input("Nhap he so b:"))
+c=int(input("Nhap he so c:"))
+print("Phuong trinh bac hai dang:", a,"X^2 + ", b,"X + ", c, "= 0")
+"""
+#Bai7
+
+
+
+
+
+
+
+
